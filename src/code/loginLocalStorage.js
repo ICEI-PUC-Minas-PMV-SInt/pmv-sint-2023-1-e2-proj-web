02676f5c87a4b591a0b2171491350e92eaaf37d6
Arthur Wilson <contact.arthurwilson@gmail.com>
2022-05-24 18:36:49 -0300

    Added LocalStorage in login page

---
+++ src/code/loginLocalStorage.js
@@ -0,0 +1,58 @@
+// Pegando a tag 
+const buttonSendForm = document.getElementById('buttonSendForm');
+
+buttonSendForm.addEventListener('click', (event) => {
+    // Prevenindo as funções que vem por padrão na tag <input>
+    event.preventDefault();
+
+    // Salvando o e-mail e a senha no localStorage
+    saveLocalStorage(userEmail, userPassword);
+
+    // Mostrando os dados do localStorage na tag <div id="localStorageOutput"></div>
+    showLocalStorage();
+});
+
+function saveLocalStorage() {
+    // Pegando os valores dos campos de "e-mail" e "senha"
+    const userEmail = document.getElementById('userEmail').value;
+    const userPassword = document.getElementById('userPassword').value;
+
+    if (userEmail === '' || userPassword === '') {
+        console.log("Algum campo não foi preenchido.");
+        return ;
+    }
+
+    // Colocando/Persistindo os dados de "e-mail" e "senha" no localStorage
+    localStorage.setItem('userEmail', userEmail);
+    localStorage.setItem('userPassword', userPassword);
+}
+
+function showLocalStorage() {
+    // Pegando a tag <div id="localStorageOutput"></div> com Javascript
+    const localStorageOutput = document.getElementById('localStorageOutput');
+
+    // Colocando o "e-mail" e a "senha" armazenados no localStorage em constantes com Javascript
+    const userEmail = localStorage.getItem("userEmail");
+    const userPassword = localStorage.getItem("userPassword");
+
+    if (userEmail === null || userPassword === null) return;
+
+    // Armazenando o conteúdo HTML em uma variável para melhorar a visualização.
+    const htmlContent = `E-mail: ${userEmail}<br>Senha: ${userPassword}<br>`;
+    const buttonClearLocalStorage = `<button onclick="clearLocalStorage()">Limpar LocalStorage</button>`;
+
+    // Colocando o "e-mail" e a "senha" dentro da tag <div id="localStorageOutput"></div>
+    localStorageOutput.innerHTML = `${htmlContent}${buttonClearLocalStorage}`;
+}
+
+function clearLocalStorage() {
+    // Criando o botão para limpar o LocalStorage
+    const buttonClearLocalStorage = `<button onclick="clearLocalStorage()">Limpar LocalStorage</button>`;
+
+    // Pegando a tag <div id="localStorageOutput"></div> com Javascript
+    const localStorageOutput = document.getElementById('localStorageOutput');
+    localStorageOutput.innerHTML = `${buttonClearLocalStorage}`;
+
+    // Limpando todo o localStorage com a função localStorage.clear()
+    localStorage.clear();
+}
